8b3bb9d4f5bcdb931d4717bf0ef73d01b937c897
Keegan <63989752+keeganwut@users.noreply.github.com>
2024-07-10 11:28:00 -0400

    feat(manifest): Add version, description, and author
    - Set initial version to 0.1.0 using semantic versioning
    - Add description: "Character Creator"
    - Include author field
    
    This commit enhances the fxmanifest.lua with key metadata,
    improving resource identification and version tracking.

--- [framework]/character-creation/fxmanifest.lua
+++ [framework]/character-creation/fxmanifest.lua
@@ -2,6 +2,10 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+author 'Keegan'
+description 'Character Creator'
+version '0.1.0'
+
 client_scripts {
     'client/*.lua',
 }
